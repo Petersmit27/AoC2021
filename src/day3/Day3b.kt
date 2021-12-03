2f51c30cdc4473d0352014f82e4ef9d2b4491748
Peter Smit <43498358+Petersmit27@users.noreply.github.com>
2021-12-03 10:05:37 +0100

    day 3b another fix :pensive:

--- src/day3/Day3b.kt
+++ src/day3/Day3b.kt
@@ -8,7 +8,7 @@ fun main() {
     var oxygenList = diagnostics
     var i = 0
     while (oxygenList.size > 1) {
-        oxygenList = if (oxygenList.count { it[i] == '1' } >= oxygenList.size / 2) {
+        oxygenList = if (oxygenList.count { it[i] == '1' } >= oxygenList.count { it[i] == '0' }) {
             oxygenList.filter { it[i] == '1' }
         } else {
             oxygenList.filter { it[i] == '0' }
@@ -19,7 +19,7 @@ fun main() {
     var co2sList = diagnostics
     i = 0
     while (co2sList.size > 1) {
-        co2sList = if (co2sList.count { it[i] == '1' } >= co2sList.size / 2) {
+        co2sList = if (co2sList.count { it[i] == '1' } >= co2sList.count { it[i] == '0' }) {
             co2sList.filter { it[i] == '0' }
         } else {
             co2sList.filter { it[i] == '1' }
