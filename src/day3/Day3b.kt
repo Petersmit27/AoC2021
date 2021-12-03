5d86e57220a53f9b3d410827c33d57456d641313
Peter Smit <43498358+Petersmit27@users.noreply.github.com>
2021-12-03 09:30:39 +0100

    day 3

---
+++ src/day3/Day3b.kt
@@ -0,0 +1,27 @@
+package day3
+
+import java.io.File
+import kotlin.math.pow
+
+fun main() {
+    val diagnostics = File("src/day3/input.txt").readLines()
+
+    var oxygenList = diagnostics
+    var co2sList = diagnostics
+    var i = 0
+    while (oxygenList.size > 1) {
+        oxygenList = if (oxygenList.count { it[i] == '1' } >= oxygenList.size / 2) {
+            oxygenList.filter { it[i] == '1' }
+        } else {
+            oxygenList.filter { it[i] == '0' }
+        }
+
+        co2sList = if (co2sList.count { it[i] == '1' } >= co2sList.size / 2) {
+            co2sList.filter { it[i] == '0' }
+        } else {
+            co2sList.filter { it[i] == '1' }
+        }
+        i++
+    }
+    println(oxygenList[0].toInt(2) * co2sList[0].toInt(2))
+}
