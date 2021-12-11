d11bfe329ea4a49dc17747aca4c37dcf81bc9765
Peter Smit <43498358+Petersmit27@users.noreply.github.com>
2021-12-11 20:35:43 +0100

    day 11

---
+++ src/day11/Day11b.kt
@@ -0,0 +1,27 @@
+package day11
+
+import java.io.File
+
+fun main() {
+    val lines =
+        File("src/day11/input.txt").readLines().map { it.map { it.digitToInt() }.toMutableList() }
+
+    val field = OctoField(lines)
+
+    var step = 1
+    while (true) {
+        field.increaseAll()
+
+        while (field.anyFlashing()) {
+            val (x, y) = field.firstFlashingOrNull()
+            field.flash(x, y)
+        }
+
+        if (field.allFlashed()) {
+            println(step)
+            return
+        }
+        step++
+    }
+
+}
